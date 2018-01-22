e758efea15866dc8e93d20ec128c23f269fc3b09
K8161 <K8161@student.jamk.fi>
2018-01-22 11:13:39 +0200

    basic movement added

--- udemydemo/state0.js
+++ udemydemo/state0.js
@@ -1,15 +1,37 @@
 var demo = {};
+var centerX = 1500 / 2;
+var centerY = 1000 / 2;
+var venom;
+var speed = 4;
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('venom', 'assets/sprites/Venom.png');
+    },
     create: function(){
         game.stage.backgroundColor = '#80ff80';
-        console.log('state0');
-        //game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1);
-        
+        console.log('state0');       
         addChangeStateEventListeners();
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        venom = game.add.sprite(centerX, centerY, 'venom')
+        venom.anchor.setTo(0.5, 0.5);
+        
     },
-    update: function(){}
+    update: function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            venom.x += speed;
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            venom.x -= speed;
+        }
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            venom.y -= speed;
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            venom.y += speed;
+        }
+    }
 };
 
 function changeState(i, stateNum){
