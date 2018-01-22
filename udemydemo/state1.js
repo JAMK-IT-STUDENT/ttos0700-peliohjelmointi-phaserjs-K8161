63e81239a3494196881d3e7c9b1e636e0cfc0fa0
K8161 <K8161@student.jamk.fi>
2018-01-22 10:36:18 +0200

    Udemy demo basics

---
+++ udemydemo/state1.js
@@ -0,0 +1,10 @@
+demo.state1 = function(){};
+demo.state1.prototype = {
+    preload: function(){},
+    create: function(){
+        game.stage.backgroundColor = '#80ff80';
+        console.log('state1');
+        addChangeStateEventListeners();
+    },
+    update: function(){}
+};
